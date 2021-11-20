7e3483a65e75d44307745fcf3a16205930bccd12
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-11-20 16:25:01 +0100

    Add translate constants

--- constants.py
+++ constants.py
@@ -5,3 +5,7 @@ FRAME_RATE = 30
 BRIGHTNESS = 150
 INTEGRATED_CAMERA = 0
 USB_CAMERA = 1
+
+# Translate model
+TO_VALUES = {0: '2', 1: '3', 2: '4', 3: '5', 4: '6', 5: '7', 6: '8', 7: '9', 8: '10', 9: 'a', 10: 'j', 11: 'k', 12: 'q'}
+TO_COLORS = {0: 'club', 1: 'diamond', 2: 'heart', 3: 'spade'}
